5a9bddb76d6c1dd3b6732c65ee030934cc680496
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-27 22:19:23 +0200

    Added boot and fs config

--- morph/simple.nix
+++ morph/simple.nix
@@ -17,20 +17,56 @@ in
   "cf-ddns" = _: {
     deployment.tags = [ "ddns" ];
     system.stateVersion = "25.05";
+
+    # LXC Containers don't boot
+    boot.loader.grub.enable = false;
+
+    fileSystems."/" = {
+      device = "none";
+      fsType = "rootfs";
+      options = [ "rw" "relatime" ];
+    };
   };
 
   "caddy" = _: {
     deployment.tags = [ "web" ];
     system.stateVersion = "25.05";
+
+    # LXC Containers don't boot
+    boot.loader.grub.enable = false;
+
+    fileSystems."/" = {
+      device = "none";
+      fsType = "rootfs";
+      options = [ "rw" "relatime" ];
+    };
   };
 
   "syncthing" = _: {
     deployment.tags = [ "svc" ];
     system.stateVersion = "25.05";
+
+    # LXC Containers don't boot
+    boot.loader.grub.enable = false;
+
+    fileSystems."/" = {
+      device = "none";
+      fsType = "rootfs";
+      options = [ "rw" "relatime" ];
+    };
   };
 
   "minecraft" = _: {
     deployment.tags = [ "svc" ];
     system.stateVersion = "25.05";
+
+    # LXC Containers don't boot
+    boot.loader.grub.enable = false;
+
+    fileSystems."/" = {
+      device = "none";
+      fsType = "rootfs";
+      options = [ "rw" "relatime" ];
+    };
   };
 }
