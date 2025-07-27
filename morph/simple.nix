bd554b045569c5682a4fd0675fca65760da05916
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-27 22:38:07 +0200

    Order might matter?

--- morph/simple.nix
+++ morph/simple.nix
@@ -18,6 +18,12 @@ in
     deployment.tags = [ "ddns" ];
     system.stateVersion = "25.05";
 
+    # Fix for mount bug in NixOS + LXC
+    systemd.mounts = [{
+      where = "/sys/kernel/debug";
+      enable = false;
+    }];
+
     # LXC Containers don't boot
     boot.loader.grub.enable = false;
 
@@ -26,18 +32,18 @@ in
       fsType = "rootfs";
       options = [ "rw" "relatime" ];
     };
-
-    # Fix for mount bug in NixOS + LXC
-    systemd.mounts = [{
-      where = "/sys/kernel/debug";
-      enable = false;
-    }];
   };
 
   "caddy" = _: {
     deployment.tags = [ "web" ];
     system.stateVersion = "25.05";
 
+    # Fix for mount bug in NixOS + LXC
+    systemd.mounts = [{
+      where = "/sys/kernel/debug";
+      enable = false;
+    }];
+
     # LXC Containers don't boot
     boot.loader.grub.enable = false;
 
@@ -46,18 +52,18 @@ in
       fsType = "rootfs";
       options = [ "rw" "relatime" ];
     };
-
-    # Fix for mount bug in NixOS + LXC
-    systemd.mounts = [{
-      where = "/sys/kernel/debug";
-      enable = false;
-    }];
   };
 
   "syncthing" = _: {
     deployment.tags = [ "svc" ];
     system.stateVersion = "25.05";
 
+    # Fix for mount bug in NixOS + LXC
+    systemd.mounts = [{
+      where = "/sys/kernel/debug";
+      enable = false;
+    }];
+
     # LXC Containers don't boot
     boot.loader.grub.enable = false;
 
@@ -66,18 +72,18 @@ in
       fsType = "rootfs";
       options = [ "rw" "relatime" ];
     };
-
-    # Fix for mount bug in NixOS + LXC
-    systemd.mounts = [{
-      where = "/sys/kernel/debug";
-      enable = false;
-    }];
   };
 
   "minecraft" = _: {
     deployment.tags = [ "svc" ];
     system.stateVersion = "25.05";
 
+    # Fix for mount bug in NixOS + LXC
+    systemd.mounts = [{
+      where = "/sys/kernel/debug";
+      enable = false;
+    }];
+
     # LXC Containers don't boot
     boot.loader.grub.enable = false;
 
@@ -86,11 +92,5 @@ in
       fsType = "rootfs";
       options = [ "rw" "relatime" ];
     };
-
-    # Fix for mount bug in NixOS + LXC
-    systemd.mounts = [{
-      where = "/sys/kernel/debug";
-      enable = false;
-    }];
   };
 }
