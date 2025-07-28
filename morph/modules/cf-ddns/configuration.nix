7c28e86d9f5cafb90a3ee458812e7deefcfd5545
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-28 18:26:25 +0200

    Attempting with derivation

--- morph/modules/cf-ddns/configuration.nix
+++ morph/modules/cf-ddns/configuration.nix
@@ -6,17 +6,18 @@
 }:
 
 let
-  scriptFile = builtins.readFile ./script.sh;
-  settingsFile = builtins.readFile ./settings;
-  settingsFileDerivation = pkgs.writeText "cf-ddns-settings" settingsFile;
-  settingsTargetPath = "/opt/cf_ddns/settings";
-  wrapperScript = pkgs.writeShellScript "cloudflare-ddns-wrapper" ''
+  scriptFileContent = builtins.readFile ./script.sh;
+  scriptDerivation = pkgs.writeShellScript "cloudflare-ddns" scriptFileContent;
+  scriptWrapper = pkgs.writeShellScript "cloudflare-ddns-wrapper" ''
     # Add binaries required to PATH
     export PATH="${pkgs.bash}/bin:${pkgs.curl}/bin:${pkgs.gnused}/bin:${pkgs.jq}/bin:$PATH"
 
-    exec ${pkgs.writeText "cloudflare-ddns-script" scriptFile} "$@"
+    exec ${pkgs.writeText "cloudflare-ddns-script" scriptDerivation} "$@"
   '';
 
+  settingsFile = builtins.readFile ./settings;
+  settingsFileDerivation = pkgs.writeText "cf-ddns-settings" settingsFile;
+  settingsTargetPath = "/opt/cf_ddns/settings";
 in
 {
   options.services.cloudflare-ddns = {
@@ -39,7 +40,7 @@ in
         ln -sf ${settingsFileDerivation} ${settingsTargetPath}
       '';
 
-      script = "${wrapperScript}";
+      script = "${scriptWrapper}";
 
       serviceConfig = {
         Type = "oneshot";
