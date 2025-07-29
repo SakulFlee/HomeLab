f5b7c8c760d477bc6a6ce31b24dbbcdc1e8f7224
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-29 18:32:48 +0200

    Attempting to use mkIf

--- morph/modules/minecraft/configuration.nix
+++ morph/modules/minecraft/configuration.nix
@@ -1,99 +1,101 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, pkgs, lib, ... }:
 
 let
-  minecraftVersion = "1.21.6";
-  paperBuild = "17";
-  memoryLimit = "8G";
-
-  pluginsJarsPath = lib.cleanSource ./plugins;
-  paperJarDerivation = pkgs.stdenv.mkDerivation {
-    pname = "paper-jar";
-    version = "${minecraftVersion}-b${paperBuild}";
-
-    src = pkgs.fetchurl {
-      url = "https://api.papermc.io/v2/projects/paper/versions/${minecraftVersion}/builds/${paperBuild}/downloads/paper-${minecraftVersion}-${paperBuild}.jar";
-    };
-
-    installPhase = ''
-      mkdir -p $out/jar
-      cp $src $out/jar/server.jar
-    '';
-  };
-
-  java = pkgs.temurin-jre-bin;
-
   minecraftServerDataDir = "/opt/minecraft";
+  java = pkgs.temurin-bin-21-jre;
 in
 {
-  services.minecraft = {
-    enable = true;
-    description = "Minecraft Paper Server v${minecraftVersion} Build ${paperBuild}";
-
-    user = "minecraft";
-    group = "minecraft";
-
-    serviceConfig = {
-      Type = "simple";
-
-      WorkingDirectory = minecraftServerDataDir;
-
-      ExecStart = "${java}/bin/java -Xms512M -Xmx${memoryLimit} -jar ${minecraftServerDataDir}/server.jar nogui";
-
-      Restart = "on-failure";
-      RestartSec = "10s";
-      MemoryMax = memoryLimit; # Match Java -Xmx
-      CPUAccounting = true;
-      IOAccounting = true;
-      StandardInput = "tty"; # Needed for console input via `systemctl attach minecraft`
-      StandardOutput = "journal";
-      StandardError = "journal";
-
-      # Pre-start commands to set up directories, EULA, and manage plugin JARs
-      ExecStartPre = ''
-        # Create directory and set permissions
-        mkdir -p "${minecraftServerDataDir}"
-        chown -R minecraft:minecraft "${minecraftServerDataDir}"
-
-        # Copy server jar
-        cp "${paperJarDerivation}/jar/server.jar" "${minecraftServerDataDir}/server.jar"
-
-        # Create plugin directory and set permissions
-        mkdir -p "${minecraftServerDataDir}/plugins"
-        chown -R minecraft:minecraft "${minecraftServerDataDir}/plugins"
-
-        # Delete all existing plugin JARs
-        find "${minecraftServerDataDir}/plugins" -maxdepth 1 -name "*.jar" -delete || true
-
-        # Copy expected plugin JARs
-        cp -r "${pluginsJarsPath}/." "${minecraftServerDataDir}/plugins/" || true
-
-        # Handle EULA
-        echo "eula=true" > "${minecraftServerDataDir}/eula.txt"
-      '';
+  options.services.minecraft = {
+    enable = lib.mkEnableOption "Minecraft Paper server";
+    description = lib.mkOption { 
+      type = lib.types.str; 
+      default = "Minecraft Paper Server"; 
+    };
+    version = lib.mkOption { 
+      type = lib.types.str; 
+      default = "1.21.6"; 
+    };
+    build = lib.mkOption { 
+      type = lib.types.int; 
+      default = 17; 
+    };
+    memoryLimit = lib.mkOption { 
+      type = lib.types.str; 
+      default = "8G"; 
+    };
+    pluginsJarsSource = lib.mkOption {
+      type = lib.types.path;
+      description = "Path to the directory containing plugin JAR files.";
     };
   };
 
-  users.groups.minecraft = { };
-  users.users.minecraft = {
-    isSystem = true;
-    group = "minecraft";
-  };
+  config = lib.mkIf config.services.minecraft.enable {
+    let
+      paperJarDerivation = pkgs.stdenv.mkDerivation {
+        pname = "server-jar";
+        version = "${config.services.minecraft.version}-b${toString config.services.minecraft.build}";
 
-  systemd.tmpfiles.rules = [
-    "d '${minecraftServerDataDir}' 0755 minecraft minecraft -"
-    "d '${minecraftServerDataDir}/plugins' 0755 minecraft minecraft -"
-  ];
+        src = pkgs.fetchurl {
+          url = "https://api.papermc.io/v2/projects/paper/versions/${config.services.minecraft.version}/builds/${toString config.services.minecraft.build}/downloads/paper-${config.services.minecraft.version}-${toString config.services.minecraft.build}.jar";
+        };
+        installPhase = "mkdir -p $out/jar; cp $src $out/jar/server.jar;";
+      };
+    in
+    {
+      systemd.services.minecraft = {
+        description = config.services.minecraft.description + " v${config.services.minecraft.version} Build ${toString config.services.minecraft.build}";
+        serviceConfig = {
+          Type = "simple";
+          DynamicUser = true;
+          WorkingDirectory = minecraftServerDataDir;
+          ExecStart = "${java}/bin/java -Xms512M -Xmx${config.services.minecraft.memoryLimit} -jar ${minecraftServerDataDir}/server.jar nogui";
+          Restart = "on-failure";
+          RestartSec = "10s";
+          MemoryMax = config.services.minecraft.memoryLimit;
+          CPUAccounting = true;
+          IOAccounting = true;
+          StandardInput = "tty";
+          StandardOutput = "journal";
+          StandardError = "journal";
 
-  networking.firewall = {
-    allowedTCPPorts = [
-      25565
-      8100
-    ];
-    allowedUDPPorts = [ 19132 ];
-  };
+          ExecStartPre = ''
+            mkdir -p "${minecraftServerDataDir}"
+            chown -R minecraft:minecraft "${minecraftServerDataDir}"
+
+            cp "${paperJarDerivation}/jar/server.jar" "${minecraftServerDataDir}/server.jar"
+
+            mkdir -p "${minecraftServerDataDir}/plugins"
+            chown -R minecraft:minecraft "${minecraftServerDataDir}/plugins"
+
+            find "${minecraftServerDataDir}/plugins" -maxdepth 1 -name "*.jar" -delete || true
+
+            cp -r "${config.services.minecraft.pluginsJarsSource}/." "${minecraftServerDataDir}/plugins/" || true
+
+            if [ ! -f "${minecraftServerDataDir}/eula.txt" ]; then
+              echo "eula=true" > "${minecraftServerDataDir}/eula.txt"
+            fi
+          '';
+        };
+      };
+
+      users.groups.minecraft = { };
+      users.users.minecraft = { isSystem = true; group = "minecraft"; };
+      systemd.tmpfiles.rules = [
+        "d '${minecraftServerDataDir}' 0755 minecraft minecraft -"
+        "d '${minecraftServerDataDir}/plugins' 0755 minecraft minecraft -"
+      ];
+      networking.firewall = {
+        allowedTCPPorts = [ 
+          # Minecraft Java
+          25565 
+          # BlueMap
+          8100 
+        ];
+        allowedUDPPorts = [ 
+          # Minecraft Bedrock
+          19132 
+        ];
+      };
+    };
+  }
 }
