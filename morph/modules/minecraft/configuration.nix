00e8854002b40266e9aa7b744f043e553b2f9719
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-29 18:06:05 +0200

    Trying to add Minecraft deployment

---
+++ morph/modules/minecraft/configuration.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  pluginsJarsPath = lib.cleanSource ../files/minecraft_plugins;
+in
+{
+  deployment.tags = [ "minecraft" ];
+
+  imports = [
+    ../common/state.nix
+    ../common/lxc.nix
+    ../common/gc.nix
+    ../common/networking.nix
+    ../common/ssh.nix
+    ../common/common_packages.nix
+    ../../modules/minecraft/minecraft-module.nix
+  ];
+
+  services.minecraft = {
+    enable = true;
+    version = "1.21.6";
+    build = 17;
+    memoryLimit = "8G";
+    pluginsJarsSource = pluginsJarsPath;
+  };
+}
