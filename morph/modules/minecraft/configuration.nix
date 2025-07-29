6a125ae5b8c9c82d4a25a927db25c68b0637e69b
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-29 18:35:33 +0200

    Trying with inside mkIf

--- morph/modules/minecraft/configuration.nix
+++ morph/modules/minecraft/configuration.nix
@@ -1,27 +1,32 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   minecraftServerDataDir = "/opt/minecraft";
-  java = pkgs.temurin-bin-21-jre;
+  java = pkgs.temurin-bin-21-jre; # Using pkgs.temurin-bin-21-jre as previously discussed
 in
 {
   options.services.minecraft = {
     enable = lib.mkEnableOption "Minecraft Paper server";
-    description = lib.mkOption { 
-      type = lib.types.str; 
-      default = "Minecraft Paper Server"; 
+    description = lib.mkOption {
+      type = lib.types.str;
+      default = "Minecraft Paper Server";
     };
-    version = lib.mkOption { 
-      type = lib.types.str; 
-      default = "1.21.6"; 
+    version = lib.mkOption {
+      type = lib.types.str;
+      default = "1.21.6";
     };
-    build = lib.mkOption { 
-      type = lib.types.int; 
-      default = 17; 
+    build = lib.mkOption {
+      type = lib.types.int;
+      default = 17;
     };
-    memoryLimit = lib.mkOption { 
-      type = lib.types.str; 
-      default = "8G"; 
+    memoryLimit = lib.mkOption {
+      type = lib.types.str;
+      default = "8G";
     };
     pluginsJarsSource = lib.mkOption {
       type = lib.types.path;
@@ -29,21 +34,29 @@ in
     };
   };
 
-  config = lib.mkIf config.services.minecraft.enable {
+  # The 'config' attribute is assigned the result of lib.mkIf
+  config = lib.mkIf config.services.minecraft.enable (
+    # The 'let ... in { ... }' block is now wrapped in parentheses,
+    # making it a single expression (an attribute set) for lib.mkIf.
     let
       paperJarDerivation = pkgs.stdenv.mkDerivation {
-        pname = "server-jar";
+        pname = "paper-jar"; # Corrected name
         version = "${config.services.minecraft.version}-b${toString config.services.minecraft.build}";
 
         src = pkgs.fetchurl {
           url = "https://api.papermc.io/v2/projects/paper/versions/${config.services.minecraft.version}/builds/${toString config.services.minecraft.build}/downloads/paper-${config.services.minecraft.version}-${toString config.services.minecraft.build}.jar";
+          # IMPORTANT: Replace with the actual SHA256!
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # <--- REPLACE THIS
         };
         installPhase = "mkdir -p $out/jar; cp $src $out/jar/server.jar;";
       };
     in
     {
+      # This is the attribute set that gets merged by lib.mkIf
       systemd.services.minecraft = {
-        description = config.services.minecraft.description + " v${config.services.minecraft.version} Build ${toString config.services.minecraft.build}";
+        description =
+          config.services.minecraft.description
+          + " v${config.services.minecraft.version} Build ${toString config.services.minecraft.build}";
         serviceConfig = {
           Type = "simple";
           DynamicUser = true;
@@ -78,24 +91,25 @@ in
         };
       };
 
+      # --- These are now within the conditionally merged attribute set ---
       users.groups.minecraft = { };
-      users.users.minecraft = { isSystem = true; group = "minecraft"; };
+      users.users.minecraft = {
+        isSystem = true;
+        group = "minecraft";
+      };
       systemd.tmpfiles.rules = [
         "d '${minecraftServerDataDir}' 0755 minecraft minecraft -"
         "d '${minecraftServerDataDir}/plugins' 0755 minecraft minecraft -"
       ];
       networking.firewall = {
-        allowedTCPPorts = [ 
-          # Minecraft Java
-          25565 
-          # BlueMap
-          8100 
+        allowedTCPPorts = [
+          25565 # Minecraft Java
+          8100 # BlueMap
         ];
-        allowedUDPPorts = [ 
-          # Minecraft Bedrock
-          19132 
+        allowedUDPPorts = [
+          19132 # Minecraft Bedrock
         ];
       };
-    };
-  }
+    } # Close the 'in' attribute set
+  ); # Close the parenthesis for lib.mkIf
 }
