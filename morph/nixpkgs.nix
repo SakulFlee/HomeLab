e534d864fd71b68634dc6c8550fced7fc0ad11a8
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-08-04 13:28:11 +0200

    Switched to Ansible, trying CI

--- morph/nixpkgs.nix
+++
@@ -1,1 +0,0 @@
-builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz"
---
+++ roles/minecraft/files/container/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Create app dir if it doesn't exist yet and change into it
+mkdir /app
+cd /app
+
+# Remove any plugin JAR files, then copy out in-image plugins
+rm -f plugins/*.jar
+cp /plugins/* plugins/
+
+# EULA
+echo "eula=true" >/app/eula.txt
+
+# Start the server! :)
+java -Xms512M -Xmx8G -jar /paper.jar
