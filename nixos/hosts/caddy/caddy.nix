12009c204eeb79d156e599291bbb2722d63fd7ef
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-26 02:08:25 +0200

    Added actual configs to Caddy

--- nixos/hosts/caddy/caddy.nix
+++ nixos/hosts/caddy/caddy.nix
@@ -3,9 +3,60 @@
     enable = true;
     email = "dev@sakul-flee.de";
     extraConfig = ''
-      :80 {
-        header Content-Type text/plain
-        respond "Hello from NixOS on CT 200!\n"
+      sakul-flee.de, www.sakul-flee.de {
+        reverse_proxy 10.0.0.102:80
+      }
+
+      nas.sakul-flee.de {
+        reverse_proxy 192.168.178.250:9443 {
+          transport http {
+            tls_insecure_skip_verify
+          }
+        }
+      }
+
+      pve.sakul-flee.de {
+        reverse_proxy 10.0.0.1:8006 {
+          transport http {
+            tls_insecure_skip_verify
+          }
+        }
+      }
+
+      pbs.sakul-flee.de {
+        reverse_proxy 10.0.0.2:8007 {
+          transport http {
+            tls_insecure_skip_verify
+          }
+        }
+      }
+
+      forgejo.sakul-flee.de {
+        reverse_proxy 10.0.0.103:3000
+      }
+
+      woodpecker.sakul-flee.de {
+        reverse_proxy 10.0.0.104:8000
+      }
+
+      prowlarr.sakul-flee.de {
+        reverse_proxy 10.0.0.108:9696
+      }
+
+      sonarr.sakul-flee.de {
+        reverse_proxy 10.0.0.109:8989
+      }
+
+      radarr.sakul-flee.de {
+        reverse_proxy 10.0.0.110:7878
+      }
+
+      qbittorrent.sakul-flee.de {
+        reverse_proxy 10.0.0.111:8080
+      }
+
+      jellyfin.sakul-flee.de {
+        reverse_proxy 10.0.0.107:8096
       }
     '';
   };
