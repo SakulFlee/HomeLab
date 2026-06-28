09fe35eff25f5fa1b0a53c0f364cc6487dca3aa3
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-28 05:01:19 +0200

    fix: drop custom postgres auth, use default peer for local socket

--- nixos/hosts/forgejo/postgres.nix
+++ nixos/hosts/forgejo/postgres.nix
@@ -8,13 +8,7 @@
 
   services.postgresql = {
     enable = true;
-    enableTCPIP = true;
-    authentication = ''
-      local forgejo forgejo scram-sha-256
-      host  forgejo forgejo 127.0.0.1/32 scram-sha-256
-    '';
     settings = {
-      listen_addresses = lib.mkForce "127.0.0.1";
       logging_collector = true;
       log_directory = "log";
     };
