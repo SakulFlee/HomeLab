965bc5344e37fe0ac4a0f706abc03dbcb537e079
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-01 22:10:18 +0200

    fansly-recorder: new LXC container (VM 112) with systemd service for stream monitoring

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -7,9 +7,13 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fansly-recorder = {
+      url = "git+https://forgejo.sakul-flee.de/SakulFlee/FanslyRecorder.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, sops-nix }: {
+  outputs = { self, nixpkgs, sops-nix, fansly-recorder }: {
     nixosConfigurations.caddy = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit sops-nix; };
@@ -80,6 +84,12 @@
       system = "x86_64-linux";
       specialArgs = { inherit sops-nix; };
       modules = [ ./hosts/qbittorrent ];
+    };
+
+    nixosConfigurations.fansly-recorder = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit sops-nix fansly-recorder; };
+      modules = [ ./hosts/fansly-recorder ];
     };
   };
 }
