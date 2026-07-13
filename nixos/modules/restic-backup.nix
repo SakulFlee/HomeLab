66da702ccc0bc8898ddb181295661a58944ae2ca
Helm <helm@homelab>
2026-07-13 02:17:41 +0000

    revert --follow-symlinks (removed in restic 0.17, now default)

--- nixos/modules/restic-backup.nix
+++ nixos/modules/restic-backup.nix
@@ -58,8 +58,7 @@ in {
         restic -r "${repo}" --password-file "${cfg.passwordFile}" backup \
           ${builtins.concatStringsSep " " cfg.paths} \
           --tag $(cat /proc/sys/kernel/hostname) \
-          --exclude-caches \
-          --follow-symlinks
+          --exclude-caches
 
         restic -r "${repo}" --password-file "${cfg.passwordFile}" forget \
           --keep-daily 7 --keep-weekly 4 --keep-monthly 3 \
