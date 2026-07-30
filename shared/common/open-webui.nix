03bf7541251ee11215bfe5f1bd70c372a1fad91e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-30 23:17:42 +0200

    Setting Open-WebUI base url to llama-swap

--- shared/common/open-webui.nix
+++ shared/common/open-webui.nix
@@ -3,5 +3,8 @@
         enable = true;
         port = 30002;
         openFirewall = false;
+        environment = {
+            OLLAMA_API_BASE_URL = "http://127.0.0.1:30001";
+        };
     };
 }
